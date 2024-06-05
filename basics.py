80902317b45464830854caa24fcb808fa7e50a4f
Trupti Balbudhe <truptibalbudhe05@gmail.com>
2024-06-05 18:57:10 +0530

    Initial files

---
+++ basics.py
@@ -0,0 +1,48 @@
+# # print fuction
+# print("Hello world")
+
+# # type fuction
+# a = 10
+# print(type(a))
+
+# # user input 
+# name = input("Enter your name ") #returns string value
+# print("hello " , name)
+
+# # Datatypes: Numbers , Boolean , String , List , Tuple, Set , Dictionary
+# a = 10 # Number: int
+# b = 3.5 # Number: float
+# c = True # Boolean
+# d = "Tony" # String
+# e = {'a', 'b' , 'c'} # set
+# f = {"key":"value" , "age":18} # dictionary
+# g = [1 , 3, 5 , 6] # List
+# h = (1 , 2, 3 , 4) # Tuple
+# print(type(h))
+
+# # Variables: concatanation
+# x , y = "Hello" , "User"
+# z = x + " " + y
+# print(z)
+
+# # Type conversion
+# a = int(input("Enter the value of a "))
+# print("A:" , a , type(a))
+
+# # Types of Operators
+# # 1. Arithmetic operators: + , - , /, * , %, ** ,//.... 
+# # 2. Assignment operator: =, +=, -=, *=, /=, %=....
+# # 3. Comparision operator: < , >, <=, >=, ==, !=, === , !==
+# # 4. Logical operator: and , or , not
+
+# # 5. Identity operators: is , is not
+# x = ["Apple" , "Mango"]
+# y = ["Apple" , "Mango"]
+# z = x
+# print(x is not y)
+# print(x is z)
+
+# # 6. Membership operators: in , not in
+# x = ["Apple" , "Mango"]
+# print("banana" not in x)
+# print("Apple" in x)
---
+++ practice/assingment.py
@@ -0,0 +1,29 @@
+# Write a program to print a string "Hello python"
+print("Hello python")
+
+# Write a program to store one value in variable and print that variable
+age = 18
+print(age)
+
+# write a program to take value from user input and print that value
+name = input("Enter your name ")
+print("Hello" , name)
+
+# write a program to do a sum of two values
+a = 33
+b = 45
+print(a + b)
+
+# write a program to perform division using variable
+print(a / b)
+
+# write a program to print multiplication of variable
+print(a * b)
+
+# write a program to find a type of variable
+print(type(a))
+
+# write a program to take two integer values from the user and check both the values are same
+a = input("Enter value of a ")
+b = input("Enter value of b")
+print(a == b)
